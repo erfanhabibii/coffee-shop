47fba71470f290970b9ffffbdbf2466139f40ac8
Erfan Habibi <erfan.habibi6.79@gmail.com>
2023-12-09 08:13:40 +0330

    installation and configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container:{
+      "center": true,
+      "padding" : "1.5rem" 
+    }
+  },
+  plugins: [
+    require("flowbite/plugin")
+  ],
+}
